7544d417704efa60f207f380d50fc03f92f06c15
JuanPablo <jpabloaj@gmail.com>
2015-04-26 16:42:35 -0400

    post answers and question with github auth

--- public/app/app.js
+++ public/app/app.js
@@ -1,35 +1,52 @@
+var ref = new Firebase('https://unit-answers.firebaseio.com/');
 var answersRef = new Firebase('https://unit-answers.firebaseio.com/answers');
 var questionsRef = new Firebase('https://unit-answers.firebaseio.com/questions');
 
 var app = angular.module('questionApp', ['firebase']);
 
 app
-  .controller('NewQuestion', function($scope, $window, $firebaseArray){
+  .factory('Auth', ['$firebaseAuth', function($firebaseAuth) {
+      return $firebaseAuth(ref);
+    }
+  ])
+  .controller('NewQuestion', function($scope, $window, Auth, $firebaseArray){
     $scope.languages = [{name:'javascript'}];
 
     $scope.newQuestion = {
       language:$scope.languages[0]
     };
 
+    $scope.auth = Auth;
+    $scope.auth.$onAuth(function(authData){
+      $scope.authData = authData;
+    });
+
     $scope.questions = $firebaseArray(questionsRef);
 
     $scope.submitQuestion = function(){
-      $scope.questions.$add({
-        title: $scope.newQuestion.title,
-        body: $scope.newQuestion.body,
-        input: $scope.newQuestion.input,
-        expected: $scope.newQuestion.expected,
-        language: $scope.newQuestion.language.name,
-        createdAt: new Date().getTime()
-      }).then(function(p){
-        $window.location.href = "/questions/" + p.name();
-      });
-
+      if ( $scope.authData ) {
+        $scope.questions.$add({
+          title: $scope.newQuestion.title,
+          body: $scope.newQuestion.body,
+          input: $scope.newQuestion.input,
+          expected: $scope.newQuestion.expected,
+          language: $scope.newQuestion.language.name,
+          createdAt: new Date().getTime(),
+          author: $scope.authData.github.username
+        }).then(function(p){
+          $window.location.href = "/questions/" + p.name();
+        });
+      }
     };
   })
-  .controller('AnswerController', function($scope, $firebaseArray){
+  .controller('AnswerController', function($scope, Auth, $firebaseArray){
 
     var questionId = document.getElementById('questionId').getAttribute('value');
+
+    $scope.auth = Auth;
+    $scope.auth.$onAuth(function(authData){
+      $scope.authData = authData;
+    });
 
     questionsRef.child(questionId).on('value', function(snap){
       $scope.question = snap.val();
@@ -39,12 +56,15 @@ app
     });
 
     $scope.addAnswer = function(){
-      $scope.answers.$add({
-        body:$scope.newAnswer.body,
-        code:$scope.newAnswer.code,
-        question:questionId,
-        state:'waiting',
-        createdAt: new Date().getTime()
-      });
+      if ( $scope.authData) {
+        $scope.answers.$add({
+          body:$scope.newAnswer.body,
+          code:$scope.newAnswer.code,
+          question:questionId,
+          state:'waiting',
+          createdAt: new Date().getTime(),
+          author: $scope.authData.github.username
+        });
+      }
     };
   });
