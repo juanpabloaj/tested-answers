530b2d04b0a6b6f64482311fa5c910c5812c1dcd
JuanPablo <jpabloaj@gmail.com>
2015-05-01 15:19:07 -0400

    filter without lowercase

--- public/app/app.js
+++ public/app/app.js
@@ -13,7 +13,6 @@ app
   .filter('capitalize', function(){
     return function(input, scope){
       if ( input ) {
-        input = input.toLowerCase();
         return input.substring(0,1).toUpperCase() + input.substring(1);
       }
     };
