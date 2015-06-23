b6910887ea405efea81c2f0cbcf680e2ae55e61f
JuanPablo <jpabloaj@gmail.com>
2015-06-23 20:51:30 -0300

    prism from bower
    * npm postinstall hook changed by bowercopy

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    bowercopy: {
+      js:{
+        options: {
+          destPrefix: 'public/js'
+        },
+        files: {
+          'markdown.js': 'markdown/lib/markdown.js',
+          'prism.js': 'prism/prism.js',
+          'prism-python.min.js': 'prism/components/prism-python.min.js',
+        }
+      },
+      css:{
+        options: {
+          destPrefix: 'public/css'
+        },
+        files: {
+          'prism.css': 'prism/themes/prism.css',
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-bowercopy');
+};
